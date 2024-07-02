992763cb5fb31a307a9bb7e51c091cd20a2097bf
Ales Zelenik <ales.zelenik@adfinis.com>
2024-07-02 12:34:11 +0200

    curly braces

--- initenv.sh
+++ initenv.sh
@@ -4,4 +4,4 @@ python3 -m venv "$(dirname "$0")/venv/"
 
 PIP=$(dirname "$0")/venv/bin/pip
 
-$PIP install -r "$(dirname "$0")/requirements.txt"
+${PIP} install -r "$(dirname "$0")/requirements.txt"
