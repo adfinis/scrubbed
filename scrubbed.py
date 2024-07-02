d89cbf7049b74e1c4b764e07f8f2465f41db06ad
Ales Zelenik <ales.zelenik@adfinis.com>
2024-07-02 15:17:24 +0200

    add health check

--- scrubbed.py
+++ scrubbed.py
@@ -99,6 +99,11 @@ def webhook():
         return jsonify(response), 400
 
 
+@app.route("/healthz")
+def health_check():
+    return "OK", 200
+
+
 if __name__ == "__main__":
     from waitress import serve
 
