1c8dcb6d7c6719c3f36904e6c3beda6ed3a1cdf0
Ales Zelenik <ales.zelenik@adfinis.com>
2024-07-10 20:00:46 +0200

    test refactor

--- scrubbed_test.go
+++ scrubbed_test.go
@@ -7,6 +7,74 @@ import (
 	"testing"
 )
 
+// Poor man's ternary operator.
+func If[T any](cond bool, vtrue, vfalse T) T {
+	if cond {
+		return vtrue
+	}
+	return vfalse
+}
+
+func createValidAlertLabels(redacted bool) map[string]string {
+	return map[string]string{
+		"alertname":  "ProbeFailure",
+		"cluster":    If(redacted, cfg.RedactedString, "foo"),
+		"namespace":  If(redacted, cfg.RedactedString, "monitoring"),
+		"node":       If(redacted, cfg.RedactedString, "node.foo.example.com"),
+		"prometheus": If(redacted, cfg.RedactedString, "monitoring/k8s"),
+		"severity":   "critical",
+	}
+}
+
+func createValidAlertAnnotations(redacted bool) map[string]string {
+	return map[string]string{
+		"description": If(redacted, cfg.RedactedString, "Instance https://server.example.org has been down for over 5m. Job: http_checks"),
+		"summary":     If(redacted, cfg.RedactedString, "BlackBox Probe Failure: https://server.example.org"),
+	}
+}
+
+func createValidAlert(redacted bool) Alert {
+	return Alert{
+		Status:       "firing",
+		Labels:       createValidAlertLabels(redacted),
+		Annotations:  createValidAlertAnnotations(redacted),
+		StartsAt:     "2023-02-06T13:08:45.828Z",
+		EndsAt:       "0001-01-01T00:00:00Z",
+		GeneratorURL: If(redacted, cfg.RedactedString, "https://console.apps.example.com/monitoring"),
+		Fingerprint:  "1a30ba71cca2921f",
+	}
+}
+
+func createValidGroupLabels(redacted bool) map[string]string {
+	return map[string]string{
+		"namespace": If(redacted, cfg.RedactedString, "monitoring"),
+	}
+}
+
+func createValidCommonLabels(redacted bool) map[string]string {
+	return map[string]string{
+		"alertname":  "ProbeFailure",
+		"cluster":    If(redacted, cfg.RedactedString, "foo"),
+		"namespace":  If(redacted, cfg.RedactedString, "monitoring"),
+		"prometheus": If(redacted, cfg.RedactedString, "monitoring/k8s"),
+		"severity":   "critical",
+	}
+}
+
+func createValidHookMessage(redacted bool) HookMessage {
+	return HookMessage{
+		Receiver:        "default",
+		Status:          "firing",
+		Alerts:          []Alert{createValidAlert(redacted)},
+		GroupLabels:     createValidGroupLabels(redacted),
+		CommonLabels:    createValidCommonLabels(redacted),
+		ExternalURL:     If(redacted, cfg.RedactedString, "https://console.apps.example.com/monitoring"),
+		Version:         "4",
+		GroupKey:        If(redacted, cfg.RedactedString, "{}/{severity=\"critical\"}:{alertname=\"ProbeFailure\"}"),
+		TruncatedAlerts: 0,
+	}
+}
+
 var (
 	cfg = Config{
 		RedactedString: "REDACTED",
@@ -14,112 +82,16 @@ var (
 		CommonLabels:   []string{"alertname", "severity"},
 	}
 
-	validAlertLabels = map[string]string{
-		"alertname":  "ProbeFailure",
-		"cluster":    "foo",
-		"namespace":  "monitoring",
-		"node":       "node.foo.example.com",
-		"prometheus": "monitoring/k8s",
-		"severity":   "critical",
-	}
-
-	validAlertLabelsRedacted = map[string]string{
-		"alertname":  "ProbeFailure",
-		"cluster":    cfg.RedactedString,
-		"namespace":  cfg.RedactedString,
-		"node":       cfg.RedactedString,
-		"prometheus": cfg.RedactedString,
-		"severity":   "critical",
-	}
-
-	validAlertAnnotations = map[string]string{
-		"description": "Instance https://server.example.org has been down for over 5m. Job: http_checks",
-		"summary":     "BlackBox Probe Failure: https://server.example.org",
-	}
-
-	validAlertAnnotationsRedacted = map[string]string{
-		"description": cfg.RedactedString,
-		"summary":     cfg.RedactedString,
-	}
-
-	validAlert = Alert{
-		Status:       "firing",
-		Labels:       validAlertLabels,
-		Annotations:  validAlertAnnotations,
-		StartsAt:     "2023-02-06T13:08:45.828Z",
-		EndsAt:       "0001-01-01T00:00:00Z",
-		GeneratorURL: "https://console.apps.example.com/monitoring",
-		Fingerprint:  "1a30ba71cca2921f",
-	}
-
-	validAlertRedacted = Alert{
-		Status:       "firing",
-		Labels:       validAlertLabelsRedacted,
-		Annotations:  validAlertAnnotationsRedacted,
-		StartsAt:     "2023-02-06T13:08:45.828Z",
-		EndsAt:       "0001-01-01T00:00:00Z",
-		GeneratorURL: cfg.RedactedString,
-		Fingerprint:  "1a30ba71cca2921f",
-	}
-
-	validGroupLabels = map[string]string{
-		"namespace": "monitoring",
-	}
-
-	validGroupLabelsRedacted = map[string]string{
-		"namespace": cfg.RedactedString,
-	}
-
-	validCommonLabels = map[string]string{
-		"alertname":  "ProbeFailure",
-		"cluster":    "foo",
-		"namespace":  "monitoring",
-		"prometheus": "monitoring/k8s",
-		"severity":   "critical",
-	}
-
-	validCommonLabelsRedacted = map[string]string{
-		"alertname":  "ProbeFailure",
-		"cluster":    cfg.RedactedString,
-		"namespace":  cfg.RedactedString,
-		"prometheus": cfg.RedactedString,
-		"severity":   "critical",
-	}
-
-	validHookMessage = HookMessage{
-		Receiver:        "default",
-		Status:          "firing",
-		Alerts:          []Alert{validAlert},
-		GroupLabels:     validGroupLabels,
-		CommonLabels:    validCommonLabels,
-		ExternalURL:     "https://console.apps.example.com/monitoring",
-		Version:         "4",
-		GroupKey:        "{}/{severity=\"critical\"}:{alertname=\"ProbeFailure\"}",
-		TruncatedAlerts: 0,
-	}
-
-	validHookMessageRedacted = HookMessage{
-		Receiver:        "default",
-		Status:          "firing",
-		Alerts:          []Alert{validAlertRedacted},
-		GroupLabels:     validGroupLabelsRedacted,
-		CommonLabels:    validCommonLabelsRedacted,
-		ExternalURL:     cfg.RedactedString,
-		Version:         "4",
-		GroupKey:        cfg.RedactedString,
-		TruncatedAlerts: 0,
-	}
-
-	validHookMessageString = `{"version":"4","groupKey":"{}/{severity=\"critical\"}:{alertname=\"ProbeFailure\"}","truncatedAlerts":0,"status":"firing","receiver":"default","groupLabels":{"namespace":"monitoring"},"commonLabels":{"alertname":"ProbeFailure","cluster":"foo","namespace":"monitoring","prometheus":"monitoring/k8s","severity":"critical"},"commonAnnotations":null,"externalURL":"https://console.apps.example.com/monitoring","alerts":[{"status":"firing","labels":{"alertname":"ProbeFailure","cluster":"foo","namespace":"monitoring","node":"node.foo.example.com","prometheus":"monitoring/k8s","severity":"critical"},"annotations":{"description":"Instance https://server.example.org has been down for over 5m. Job: http_checks","summary":"BlackBox Probe Failure: https://server.example.org"},"startsAt":"2023-02-06T13:08:45.828Z","EndsAt":"0001-01-01T00:00:00Z","generatorURL":"https://console.apps.example.com/monitoring","fingerprint":"1a30ba71cca2921f"}]}`
+	validHookMessageString = `{"version":"4","groupKey":"{}/{severity=\"critical\"}:{alertname=\"ProbeFailure\"}","truncatedAlerts":0,"status":"firing","receiver":"default","groupLabels":{"namespace":"monitoring"},"commonLabels":{"alertname":"ProbeFailure","cluster":"foo","namespace":"monitoring","prometheus":"monitoring/k8s","severity":"critical"},"commonAnnotations":null,"externalURL":"https://console.apps.example.com/monitoring","alerts":[{"status":"firing","labels":{"alertname":"ProbeFailure","cluster":"foo","namespace":"monitoring","node":"node.foo.example.com","prometheus":"monitoring/k8s","severity":"critical"},"annotations":{"description":"Instance https://server.example.org has been down for over 5m. Job: http_checks","summary":"BlackBox Probe Failure: https://server.example.org"},"startsAt":"2023-02-06T13:08:45.828Z","endsAt":"0001-01-01T00:00:00Z","generatorURL":"https://console.apps.example.com/monitoring","fingerprint":"1a30ba71cca2921f"}]}`
 )
 
 func TestToJSONStringValid(t *testing.T) {
 	t.Parallel()
 
-	resultString := toJSONString(validHookMessage)
+	resultString := toJSONString(createValidHookMessage(false))
 
 	if resultString != validHookMessageString {
-		t.Fatalf(`toJSONString(%v) = %s, want "%s"`, validHookMessage, resultString, validHookMessageString)
+		t.Fatalf(`toJSONString(%v) = %s, want "%s"`, createValidHookMessage(false), resultString, validHookMessageString)
 	}
 }
 
@@ -137,22 +109,22 @@ func TestToJSONStringNil(t *testing.T) {
 func TestRedactFields(t *testing.T) {
 	t.Parallel()
 
-	resultMap := validAlertLabels
+	resultMap := createValidAlertLabels(false)
 	redactFields(&resultMap, cfg.AlertLabels, cfg.RedactedString)
-
-	if !reflect.DeepEqual(resultMap, validAlertLabelsRedacted) {
-		t.Fatalf(`redactFields(%v) = %v, want %v`, validAlertLabels, resultMap, validAlertLabelsRedacted)
+	expectMap := createValidAlertLabels(true)
+	if !reflect.DeepEqual(resultMap, expectMap) {
+		t.Fatalf(`redactFields(%v) = %v, want %v`, createValidAlertLabels(false), resultMap, expectMap)
 	}
 }
 
 func TestScrub(t *testing.T) {
 	t.Parallel()
 
-	resultHookMessage := validHookMessage
+	resultHookMessage := createValidHookMessage(false)
 	scrub(&resultHookMessage, cfg)
-
-	if !reflect.DeepEqual(resultHookMessage, validHookMessageRedacted) {
-		t.Fatalf(`scrub(%v) = %v, want %v`, validHookMessage, resultHookMessage, validHookMessageRedacted)
+	expectHookMessage := createValidHookMessage(true)
+	if !reflect.DeepEqual(resultHookMessage, expectHookMessage) {
+		t.Fatalf(`scrub(%v) = %v, want %v`, createValidHookMessage(false), resultHookMessage, expectHookMessage)
 	}
 }
 
